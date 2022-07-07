3c7d40b513909ae875ec52a19fe4413b0dc8362e
Koehler <ckoehler16@gmail.com>
2022-07-07 09:35:34 -0400

    comments model and routes

--- routes/api/index.js
+++ routes/api/index.js
@@ -1,5 +1,9 @@
 const router = require('express').Router();
+const commentRoutes = require('./comment-routes');
 const pizzaRoutes = require('./pizza-routes');
+
+// add prefix of `/comments` to all routes created in commentRoutes
+router.use('/comments', commentRoutes);
 
 // add prefix of `/pizzas` to routes created in `pizza-routes.js`
 router.use('/pizzas', pizzaRoutes);
